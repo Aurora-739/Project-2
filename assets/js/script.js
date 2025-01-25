245b9734e11657a0a505d1e857a289053b89f76d
Aurora Keay <yfifej9y@students.codeinstitute.net>
2025-01-25 09:27:05 +0000

    the playing cards have been added to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,32 @@
-document.addEventListener("DOMContentLoaded", function()){
-    
-//BUTTONS FOR THE ONCLCIK FUCNTION
-let higher = document.getElementById("higher");
-let lower = document.getElementById("lower");
-let buttons = document.getElementsByClassName("buttons")
+document.addEventListener("DOMContentLoaded", function(){
+        
 
-for (let button of buttons) {
-    button.addEventListener(this.onclick, function(){
-        if (getAttribute("valueOfCard") > higher) {
-            //end the loop --> display alert and add to losing score
-        } else if (getAttribute("valueOfCard") > lower)
-            //continue the loop --> display alert add to the winning score 
-    });
-}
+        
+    //BUTTONS FOR THE ONCLCIK FUCNTION
+    let higher = document.getElementById("higher");
+    let lower = document.getElementById("lower");
+    let buttons = document.getElementsByClassName("buttons")
 
-    //RANDOM IMAGE GENERATOR
-    function(buttons(onclick)) {
-        const imgEl=
+    for (let button of buttons) {
+        button.addEventListener(this.onclick, function(){
+            if (getAttribute("valueOfCard") > guess) {
+                //end the loop --> display alert and add to losing score
+            } else if (getAttribute("valueOfCard") > guess)
+                //continue the loop --> display alert add to the winning score 
+        });
     }
-    
-};
+
+        //RANDOM IMAGE GENERATOR
+        //!!!! function card() {
+            card = new image(pathImage)
+        }
+
+
+    // Count for cards --> give them true / false values
+    function guess() {
+        if (onclick(higher)) {
+            let guess = valueOfCard > card;
+        } else if (onclick(lower)) {
+            let guess = valueOfCard < card;
+        }
+    }
